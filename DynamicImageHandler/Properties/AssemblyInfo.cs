dd53a2de53b00f561214066816dd151e147a84d7
Jaben Cargman <jaben.cargman@captiveaire.com>
2017-07-21 13:47:08 -0400

    Updated to .NET v4.5.2

--- DynamicImageHandler/Properties/AssemblyInfo.cs
+++ DynamicImageHandler/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DynamicImageHandler")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
